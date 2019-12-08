e29ba524fb1bf892aa1cab8a733d20566c56cfb6
vagrant <vagrant@localhost.localdomain>
2019-12-09 08:54:51 +0900

    added routes signup login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
 
     validates :name, {presence: true}
     validates :email, {presence: true, uniqueness: true}
+
+    # def messages
+    #     return Message.where(user_id: self.id)
+    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  get 'messages/index'
+  get 'messages/index' => "messages#index"
 
-  get 'users/index'
+  get 'users/index' => "users#index"
+  get "signup" => "users#new"
+  post "users/create" => "users#create"
+  get "login" => "users#login_form"
+  post "login" => "users#login"
 
   get 'teacher/top'
 
   get "/" => "home#top"
+  
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
