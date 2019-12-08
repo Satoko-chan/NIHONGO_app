0112cffc9a415cb826ad921322e0b9981c1f2a1b
vagrant <vagrant@localhost.localdomain>
2019-12-09 07:38:47 +0900

    added Message table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191208211922) do
+ActiveRecord::Schema.define(version: 20191208223451) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "chat_check"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
