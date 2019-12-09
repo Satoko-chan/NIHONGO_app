ab209445c9c41e6499c77c3195cf8db55acfc4a7
vagrant <vagrant@localhost.localdomain>
2019-12-10 03:21:24 +0900

    added users_controller signin login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,45 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+      name: params[:name], 
+      email: params[:email], 
+      skype_name: params[:skype_name], 
+      password: params[:password])
+    if @user.save
+      # session
+      # flash
+      redirect_to("/users/#{@user.id}/student")
+    else
+      @name = params[:name]
+      @email = params[:email]
+      @skype_name = params[:skype_name]
+      @password = params[:password]
+      render("users/new")
+    end
+  end
+
+  def login_form
+    
+  end
+
+  def login
+    @user = User.find_by(
+      email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      # session
+      # flash
+      redirect_to("/users/#{@user.id}/student")
+    else
+      # error_message
+      @email = params[:email]
+      @password = params[:password]
+      render("users/login_form")
+    end
   end
 
   def show
